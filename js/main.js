90f016060daa44a94860e685968953c68ebc6df1
enzo <enzovcnt@gmail.com>
2024-11-20 19:03:16 +0100

    name info appear

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-const apiUrl = ('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6dcc83c9ec6deb27a2ac414d0af5c13d ')
+const lat = document.querySelector('.latitude');
+const long = document.querySelector('.longitude');
+const button = document.querySelector('.bouton');
+const names = document.querySelector('.name');
+const weather = document.querySelector('.weather');
+const apiUrl = (`https://api.openweathermap.org/data/2.5/weather?lat=45.133&lon=7.367&appid=6dcc83c9ec6deb27a2ac414d0af5c13d`);
 
 
-function weather (){
 
+function getWeather() {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+            names.innerHTML = data.name;
+        })
+        .catch(err => console.log(err));
 }
-fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => console.log(data))
+
+getWeather();
+
+button.addEventListener('click', getWeather);
+
+// étape 1 = pouvoir avoir URL dynamique
+// étape 2 = entrer long et lat et clicker pour que HTML change
+// étape 3 = plus d'information
