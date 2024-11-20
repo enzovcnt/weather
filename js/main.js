0cd335e64725f6bda8a3b191426bf53febfa5f0f
enzo <enzovcnt@gmail.com>
2024-11-20 17:47:01 +0100

    I have the global data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+const apiUrl = ('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6dcc83c9ec6deb27a2ac414d0af5c13d ')
+
+
+function weather (){
+
+}
+fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => console.log(data))
