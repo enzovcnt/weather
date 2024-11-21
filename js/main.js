453e56b42c1a9ead83496f281bae840463f20368
enzo <enzovcnt@gmail.com>
2024-11-21 18:09:50 +0100

    add a new data

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const names = document.querySelector('.name');
 const weather = document.querySelector('.weather');
 const temp = document.querySelector('.température');
 const humidity = document.querySelector('.humid');
+const country = document.querySelector('.country');
 const apiUrl = (`https://api.openweathermap.org/data/2.5/weather?lat=45.75&lon=4.850&units=metric&appid=6dcc83c9ec6deb27a2ac414d0af5c13d`);
 
 
@@ -15,7 +16,8 @@ function getWeather() {
         .then(res => res.json())
         .then(data => {
             names.innerHTML ='Ville :' + data.name;
-            weather.innerHTML = data.weather.main;
+            country.innerHTML ='Pays :' + data.sys.country;
+            weather.innerHTML ='Condition :' + data.weather.main;
             temp.innerHTML ='Température :' + data.main.temp + '°C';
             humidity.innerHTML ='Humidité' + data.main.humidity + '%';
         })
@@ -27,7 +29,7 @@ function getWeather() {
 button.addEventListener('click', getWeather);
 
 
-// étape 1 = plus d'information
+// étape 1 = plus d'information = finish
 // étape 2 = pouvoir avoir URL dynamique
 // étape 3 = entrer long et lat et clicker pour que HTML change
 
